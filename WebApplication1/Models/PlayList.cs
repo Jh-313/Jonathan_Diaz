efe260d7ea5f9958483454696553f4d65619d5ba
Jh-313 <jsdiazy@utn.edu.ec>
2025-07-06 00:06:00 -0500

    avances

---
+++ WebApplication1/Models/PlayList.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class PlayList
+    {
+        public string PlaylistName { get; set; }
+        public string PlaylistDescription { get; set; }
+        public string PlaylistImageUrl { get; set; }
+        public int NumberOfSongs { get; set; }
+        public List<Song> Songs { get; set; }
+    }
+
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+    }
+}
